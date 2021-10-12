d556a665a8628a696296531ea074646eeefa9ecd
Florie <florie.pons@gmail.com>
2021-10-12 17:17:34 +0200

    Creation boucle for

--- front/js/script.js
+++ front/js/script.js
@@ -14,33 +14,34 @@ dataApi
     console.table(products);
 
     try {
-      //Retour Name
-      const productName = products[0].name;
-      console.log(productName);
-      const affichage_Name = document.querySelector(".productName");
-      affichage_Name.innerHTML = productName;
 
-      //Retour Description
-      const productDescription = products[0].description;
-      console.log(productDescription);
-      const affichage_Description = document.querySelector(".productDescription");
-      affichage_Description.innerHTML = productDescription;
+      for (var i = 0; i < products.length; i++) {
 
-      //Retour Image & Alt 
-      const imgUrl = products[0].imageUrl;
-      console.log(imgUrl);
-      const imgAlt = products[0].altTxt;
-      console.log(imgAlt);
-      document.querySelector('#items img').src = imgUrl;
-      document.querySelector('#items img').alt = imgAlt;
+        //Creation nouveau <a>
+        const newElt = document.createElement("a");
+        let elt2 = document.getElementById('items');
+        elt2.appendChild(newElt);
+        newElt.innerHTML = "<a href=\"./product.html?id=42\"><article><img src=\"\" alt=\"\"><h3 class=\"productName\">Kanap name1</h3><p class=\"productDescription\">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p></article></a>";
+        //Retour Name
+        const productName = products[i].name;
+        console.log(productName);
+        const affichage_Name = document.querySelector(".productName");
+        affichage_Name.innerHTML = productName;
 
-      //Creation nouveau <a>
-      const newElt = document.createElement("a");
-      let elt2 = document.getElementById('items');
-      elt2.appendChild(newElt);
-      newElt.innerHTML = "<a href=\"./product.html?id=42\"><article><img src=\".../product01.jpg\" alt=\"Lorem ipsum dolor sit amet, Kanap name1\"><h3 class=\"productName\">Kanap name1</h3><p class=\"productDescription\">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p></article></a>";
-      /*while (var i = 0; i < products.length; i++) {
-    }*/
+        //Retour Description
+        const productDescription = products[i].description;
+        console.log(productDescription);
+        const affichage_Description = document.querySelector(".productDescription");
+        affichage_Description.innerHTML = productDescription;
+
+        //Retour Image & Alt 
+        const imgUrl = products[i].imageUrl;
+        console.log(imgUrl);
+        const imgAlt = products[i].altTxt;
+        console.log(imgAlt);
+        document.querySelector('#items img').src = imgUrl;
+        document.querySelector('#items img').alt = imgAlt;
+      }
 
       //Catch du Try
     } catch (err) {
