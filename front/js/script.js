bc30e536ee40473affa3b380c4f9ded3c238f269
Florie <florie.pons@gmail.com>
2021-10-15 12:18:51 +0200

    Modification pour implémentation dynamique

--- front/js/script.js
+++ front/js/script.js
@@ -1,61 +1,64 @@
-/*fetch('http://localhost:3000/api/products')
+//Appel de l'API
+const dataApi = fetch('http://localhost:3000/api/products');
+
+//Récupération des données de l'API
+dataApi
   .then((httpBodyResponse) => httpBodyResponse.json())
-  .then((products) => console.table(products[0]))
-  
-  .catch((error) => {
-    alert('Il y a eu un problème avec l\'opération fetch: ' + error.message)
-  });*/
+  .then((products) => {
+    console.table(products);
 
-  const dataApi = fetch('http://localhost:3000/api/products');
+    try {
 
-  dataApi
-    .then((httpBodyResponse) => httpBodyResponse.json())
-    .then((products) => {
-      console.table(products);
-  
-      try {
-  
+      //Déclaration des variables
+      let productId = []
+      let productName = [];
+      let productDescription = [];
+      let imgUrl = [];
+      let imgAlt = [];
+      let cardStructure = [];
+
+      //Création de la fonction d'affichage dynamique
+      function productDisplay(products) {
+        //sélection élément du DOM
+        const positionElement = document.querySelector('#items');
+
+        //Création de la boucle
         for (var i = 0; i < products.length; i++) {
-  
-          //Creation nouveau <a>
-          const newElt = document.createElement("a");
-          let elt2 = document.getElementById('items');
-          elt2.appendChild(newElt);
-          newElt.innerHTML = "<article><img src=\"\" alt=\"\"><h3 class=\"productName\">Kanap name1</h3><p class=\"productDescription\">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p></article>";
+          //Stockage des données dnas les variables
+          products.forEach((element, i) => {
+            productId[i] = products[i]._id;
+            productName[i] = products[i].name;
+            productDescription[i] = products[i].description;
+            imgUrl[i] = products[i].imageUrl;
+            imgAlt[i] = products[i].altTxt;
+          });
 
-          //Retour Name
-          document.querySelector('#items a').href = "./product.html?id=42";
+          //Affichage des objets dans la page
+          cardStructure += `
+			      <a href="./product.html?id=${productId[i]}">
+            	<article>
+              		<img src="${imgUrl[i]}" alt="${imgAlt[i]}">
+              		<h3 class="productName">${productName[i]}</h3>
+              		<p class="productDescription">${productDescription[i]}</p>
+            	</article>
+          	</a>
+  		    `;
 
-          const productName = products[i].name;
-          console.log(productName);
-          const affichage_Name = document.querySelector(".productName");
-          affichage_Name.innerHTML = productName;
-      
-          //Retour Description
-          const productDescription = products[i].description;
-          console.log(productDescription);
-          const affichage_Description = document.querySelector(".productDescription");
-          affichage_Description.innerHTML = productDescription;
-  
-          //Retour Image & Alt 
-          const imgUrl = products[i].imageUrl;
-          console.log(imgUrl);
-          const imgAlt = products[i].altTxt;
-          console.log(imgAlt);
-          document.querySelector('#items img').src = imgUrl;
-          document.querySelector('#items img').alt = imgAlt;
-
+          //Injection dans le HTML
+          positionElement.innerHTML = cardStructure;
         }
-
-   
-
-  
-        //Catch du Try
-      } catch (err) {
-        alert("Il y a eu un problème avec l'opération try: " + err.message)
       }
-    })
-    //Catch du Fetch
-    .catch((error) => {
-      alert("Il y a eu un problème avec l'opération fetch: " + error.message)
-    });
+
+      //Appel de la fonction d’affichage des produits 
+      productDisplay(products);
+
+      //Catch du Try
+    } catch (err) {
+      alert("Il y a eu un problème avec l'opération try: " + err.message)
+    }
+  })
+
+  //Catch du Fetch
+  .catch((error) => {
+    alert("Il y a eu un problème avec l'opération fetch: " + error.message)
+  });
